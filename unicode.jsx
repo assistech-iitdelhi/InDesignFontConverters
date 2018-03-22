5d44672482354860d173bd83cfc1a17cd89f42b2
Himanshu Garg <himanshugarg@zoho.com>
2018-03-22 18:43:40 +0530

    WIP convert everything

--- unicode.jsx
+++ unicode.jsx
@@ -1,13 +1,42 @@
-﻿for (var j = app.activeDocument.stories.length-1; j >= 0 ; j--) {    
-    for (var k = app.activeDocument.stories.item(j).textStyleRanges.length-1; k >= 0; k--) {
-        var myText = app.activeDocument.stories.item(j).textStyleRanges.item(k);        
-        myText.contents = C2Unic(myText.contents, myText.appliedFont.fontFamily);
-        myText.appliedFont = app.fonts.item("Utsaah");
-        /*myText.pointSize = myText.pointSize*0.878;
-        myText.composer="Adobe World-Ready Paragraph Composer";*/
-    }
+﻿var textStyleRanges = [];
+for (var j = app.activeDocument.stories.length-1; j >= 0 ; j--)
+  for (var k = app.activeDocument.stories.item(j).textStyleRanges.length-1; k >= 0; k--)
+    textStyleRanges.push(app.activeDocument.stories.item(j).textStyleRanges.item(k));
+
+for (var i = textStyleRanges.length-1; i >= 0; i--) {
+  var myText = textStyleRanges[i];
+  var converted = C2Unic(myText.contents, myText.appliedFont.fontFamily);
+  if (myText.contents != converted)
+    myText.contents = converted;        
+
+  if (myText.appliedFont.fontFamily == 'Chanakya' 
+  || myText.appliedFont.fontFamily ==  'DevLys 010' 
+  || myText.appliedFont.fontFamily ==  'Walkman-Chanakya-905') {          
+    myText.appliedFont = app.fonts.item("Utsaah");
+    myText.composer="Adobe World-Ready Paragraph Composer";
+  }
 }
 
+/*
+var myStories = app.activeDocument.stories;
+for (var j = myStories.length-1; j >= 0 ; j--) {    
+  var textStyleRanges = myStories.item(j).textStyleRanges;
+  for (var k = textStyleRanges.length-1; k >= 0; k--) {
+      var myText = textStyleRanges.item(k);              
+      var converted = C2Unic(myText.contents, myText.appliedFont.fontFamily);
+      if (myText.contents != converted)
+        myText.contents = converted;        
+  }
+  for (var k = 0; k < textStyleRanges.length; k++) {        
+    var myText = textStyleRanges.item(k);                
+    if (myText.appliedFont.fontFamily == 'Chanakya' 
+    || myText.appliedFont.fontFamily ==  'DevLys 010' 
+    || myText.appliedFont.fontFamily ==  'Walkman-Chanakya-905') {          
+      myText.appliedFont = app.fonts.item("Utsaah");
+      myText.composer="Adobe World-Ready Paragraph Composer";
+    }        
+  }
+}*/
 
 function C2Unic(text, font) {
     //convert text from font to unicode
@@ -17,381 +46,329 @@ function C2Unic(text, font) {
         return Devlys2Uni(text);
     else if (font == "Walkman-Chanakya-905")
         return convert_wchanakya_to_unicode(text);
-    //else
-    //    alert("Unknown font: " + font);
-    return "";
+    return text;
 }
 
 function convert_wchanakya_to_unicode(legacy_txt) {
+  var array_one = new Array(
+    "ñ" , "॰" ,
+    "Q\+Z" , "QZ\+" ,
+    "sas" , "sa" ,
+    "aa" , "a" ,
+    "¼Z" , "र्द्ध" ,
+    "ZZ" , "Z" ,
 
-var array_one = new Array(
-"ñ" , "॰" ,
-"Q\+Z" , "QZ\+" ,
-"sas" , "sa" ,
-"aa" , "a" ,
-"¼Z" , "र्द्ध" ,
-"ZZ" , "Z" ,
+    "å" , "०" ,
+    "ƒ" , "१" ,
+    "\„" , "२" ,
+    "…" , "३" ,
+    "†" , "४" ,
+    "‡" , "५" ,
+    "\ˆ" , "६" ,
+    "‰" , "७" ,
+    "Š" , "८" ,
+    "\‹" , "९" ,
+    "¶+" , "फ़्" ,
+    "d+" , "क़" ,
+    "[+k" , "ख़" ,
+    "[+" , "ख़्" ,
+    "x+" , "ग़" ,
+    "T+" , "ज़्" ,
+    "t+" , "ज़" ,
+    "M+" , "ड़" ,
+    "\<+" , "ढ़" ,
+    "Q+" , "फ़" ,
+    "\;+" , "य़" ,
+    "j+" , "ऱ" ,
+    "u+" , "ऩ" ,
+    "Ùk" , "त्त" ,
+    "Ù" , "त्त्" ,
+    "ä" , "क्त" ,
+    "–" , "दृ" ,
+    "—" , "कृ" ,
+    "é" , "न्न" ,
+    "™" , "न्न्" ,
+    "\=kk" , "\=k" ,
+    "f\=k" , "f\=" ,
+    "à" , "ह्न" ,
+    "á" , "ह्य" ,
+    "â" , "हृ" ,
+    "ã" , "ह्म" ,
+    "ºz" , "ह्र" ,
+    "º" , "ह्" ,
+    "í" , "द्द" ,
+    "\{k" , "क्ष" ,
+    "\{" , "क्ष्" ,
+    "f\=" , "त्रि" ,
+    "\=k" , "त्र" ,
+    "\«" , "त्र्" ,
+    "Nî" , "छ्य" ,
+    "Vî" , "ट्य" ,
+    "Bî" , "ठ्य" ,
+    "Mî" , "ड्य" ,
+    "\<î" , "ढ्य" ,
+    "|" , "द्य" ,
+    "K" , "ज्ञ" ,
+    "}" , "द्व" ,
+    "J" , "श्र" ,
+    "Vª" , "ट्र" ,
+    "Mª" , "ड्र" ,
+    "\>ª" , "ढ्र" ,
+    "Nª" , "छ्र" ,
+    "Ø" , "क्र" ,
+    "Ý" , "फ्र" ,
+    "nzZ" , "र्द्र" ,
+    "æ" , "द्र" ,
+    "ç" , "प्र" ,
+    "Á" , "प्र" ,
+    "xz" , "ग्र" ,
+    "#" , "रु" ,
+    ":" , "रू" ,
+    "v‚" , "ऑ" ,
+    "vks" , "ओ" ,
+    "vkS" , "औ" ,
+    "vk" , "आ" ,
+    "v" , "अ" ,
+    "b±" , "ईं" ,
+    "Ã" , "ई" ,
+    "bZ" , "ई" ,
+    "b" , "इ" ,
+    "mQ" , "ऊ" ,
+    "m" , "उ" ,
+    "Å" , "ऊ" ,
+    "\,s" , "ऐ" ,
+    "\," , "ए" ,
+    "½" , "ऋ" ,
+    "ô" , "क्क" ,
+    "d" , "क" ,
+    "Dk" , "क" ,
+    "D" , "क्" ,
+    "£" , "र्f" ,
+    "[k" , "ख" ,
+    "[" , "ख्" ,
+    "x" , "ग" ,
+    "Xk" , "ग" ,
+    "X" , "ग्" ,
+    "Ä" , "घ" ,
+    "?k" , "घ" ,
+    "?" , "घ्" ,
+    "³" , "ङ" ,
+    "p" , "च" ,
+    "Pk" , "च" ,
+    "P" , "च्" ,
 
-"å" , "०" ,
-"ƒ" , "१" ,
-"\„" , "२" ,
-"…" , "३" ,
-"†" , "४" ,
-"‡" , "५" ,
-"\ˆ" , "६" ,
-"‰" , "७" ,
-"Š" , "८" ,
-"\‹" , "९" ,
-"¶+" , "फ़्" ,
-"d+" , "क़" ,
-"[+k" , "ख़" ,
-"[+" , "ख़्" ,
-"x+" , "ग़" ,
-"T+" , "ज़्" ,
-"t+" , "ज़" ,
-"M+" , "ड़" ,
-"\<+" , "ढ़" ,
-"Q+" , "फ़" ,
-"\;+" , "य़" ,
-"j+" , "ऱ" ,
-"u+" , "ऩ" ,
-"Ùk" , "त्त" ,
-"Ù" , "त्त्" ,
-"ä" , "क्त" ,
-"–" , "दृ" ,
-"—" , "कृ" ,
-"é" , "न्न" ,
-"™" , "न्न्" ,
-"\=kk" , "\=k" ,
-"f\=k" , "f\=" ,
-"à" , "ह्न" ,
-"á" , "ह्य" ,
-"â" , "हृ" ,
-"ã" , "ह्म" ,
-"ºz" , "ह्र" ,
-"º" , "ह्" ,
-"í" , "द्द" ,
-"\{k" , "क्ष" ,
-"\{" , "क्ष्" ,
-"f\=" , "त्रि" ,
-"\=k" , "त्र" ,
-"\«" , "त्र्" ,
-"Nî" , "छ्य" ,
-"Vî" , "ट्य" ,
-"Bî" , "ठ्य" ,
-"Mî" , "ड्य" ,
-"\<î" , "ढ्य" ,
-"|" , "द्य" ,
-"K" , "ज्ञ" ,
-"}" , "द्व" ,
-"J" , "श्र" ,
-"Vª" , "ट्र" ,
-"Mª" , "ड्र" ,
-"\>ª" , "ढ्र" ,
-"Nª" , "छ्र" ,
-"Ø" , "क्र" ,
-"Ý" , "फ्र" ,
-"nzZ" , "र्द्र" ,
-"æ" , "द्र" ,
-"ç" , "प्र" ,
-"Á" , "प्र" ,
-"xz" , "ग्र" ,
-"#" , "रु" ,
-":" , "रू" ,
-"v‚" , "ऑ" ,
-"vks" , "ओ" ,
-"vkS" , "औ" ,
-"vk" , "आ" ,
-"v" , "अ" ,
-"b±" , "ईं" ,
-"Ã" , "ई" ,
-"bZ" , "ई" ,
-"b" , "इ" ,
-"mQ" , "ऊ" ,
-"m" , "उ" ,
-"Å" , "ऊ" ,
-"\,s" , "ऐ" ,
-"\," , "ए" ,
-"½" , "ऋ" ,
-"ô" , "क्क" ,
-"d" , "क" ,
-"Dk" , "क" ,
-"D" , "क्" ,
-"£" , "र्f" ,
-"[k" , "ख" ,
-"[" , "ख्" ,
-"x" , "ग" ,
-"Xk" , "ग" ,
-"X" , "ग्" ,
-"Ä" , "घ" ,
-"?k" , "घ" ,
-"?" , "घ्" ,
-"³" , "ङ" ,
-"p" , "च" ,
-"Pk" , "च" ,
-"P" , "च्" ,
+    "N" , "छ" ,
 
-"N" , "छ" ,
+    "\”k" , "ज" ,
+    "\”" , "ज्" ,
 
-"\”k" , "ज" ,
-"\”" , "ज्" ,
+    "t" , "ज" ,
+    "Tk" , "ज" ,
+    "T" , "ज्" ,
+    "\>" , "झ" ,
+    "÷" , "झ्" ,
+    "¥" , "ञ" ,
+    "ê" , "ट्ट" ,
+    "ë" , "ट्ठ" ,
+    "V" , "ट" ,
+    "B" , "ठ" ,
+    "ì" , "ड्ड" ,
+    "ï" , "ड्ढ" ,
+    "M+" , "ड़" ,
+    "\<+" , "ढ़" ,
+    "M" , "ड" ,
+    "\<" , "ढ" ,
+    "\.k" , "ण" ,
+    "\." , "ण्" ,
+    "r" , "त" ,
+    "Rk" , "त" ,
+    "R" , "त्" ,
+    "Fk" , "थ" ,
+    "F" , "थ्" ,
+    "n" , "द" ,
+    "\/" , "ध" ,
+    "èk" , "ध" ,
+    "è" , "ध्" ,
+    "Ë  " , "ध्" ,
+    "u" , "न" ,
+    "Uk" , "न" ,
+    "U" , "न्" ,
+    "iQ" , "फ" ,
+    "i" , "प" ,
+    "Ik" , "प" ,
+    "I" , "प्" ,
+    "¶" , "फ्" ,
+    "c" , "ब" ,
+    "Ck" , "ब" ,
+    "C" , "ब्" ,
+    "Hk" , "भ" ,
+    "H" , "भ्" ,
+    "e" , "म" ,
+    "Ek" , "म" ,
+    "E" , "म्" ,
+    "\;" , "य" ,
+    "\¸" , "य्" ,
+    "j" , "र" ,
+    "y" , "ल" ,
+    "Yk" , "ल" ,
+    "Y" , "ल्" ,
+    "G" , "ळ" ,
+    "oQ" , "क" ,
+    "o" , "व" ,
+    "Ok" , "व" ,
+    "O" , "व्" ,
 
-"t" , "ज" ,
-"Tk" , "ज" ,
-"T" , "ज्" ,
-"\>" , "झ" ,
-"÷" , "झ्" ,
-"¥" , "ञ" ,
-"ê" , "ट्ट" ,
-"ë" , "ट्ठ" ,
-"V" , "ट" ,
-"B" , "ठ" ,
-"ì" , "ड्ड" ,
-"ï" , "ड्ढ" ,
-"M+" , "ड़" ,
-"\<+" , "ढ़" ,
-"M" , "ड" ,
-"\<" , "ढ" ,
-"\.k" , "ण" ,
-"\." , "ण्" ,
-"r" , "त" ,
-"Rk" , "त" ,
-"R" , "त्" ,
-"Fk" , "थ" ,
-"F" , "थ्" ,
-"n" , "द" ,
-"\/" , "ध" ,
-"èk" , "ध" ,
-"è" , "ध्" ,
-"Ë  " , "ध्" ,
-"u" , "न" ,
-"Uk" , "न" ,
-"U" , "न्" ,
-"iQ" , "फ" ,
-"i" , "प" ,
-"Ik" , "प" ,
-"I" , "प्" ,
-"¶" , "फ्" ,
-"c" , "ब" ,
-"Ck" , "ब" ,
-"C" , "ब्" ,
-"Hk" , "भ" ,
-"H" , "भ्" ,
-"e" , "म" ,
-"Ek" , "म" ,
-"E" , "म्" ,
-"\;" , "य" ,
-"\¸" , "य्" ,
-"j" , "र" ,
-"y" , "ल" ,
-"Yk" , "ल" ,
-"Y" , "ल्" ,
-"G" , "ळ" ,
-"oQ" , "क" ,
-"o" , "व" ,
-"Ok" , "व" ,
-"O" , "व्" ,
+    "\'k" , "श" ,
+    "\'" , "श्" ,
 
-"\'k" , "श" ,
-"\'" , "श्" ,
+    "Ük" , "श" ,
+    "Ü" , "श्" ,
 
-"Ük" , "श" ,
-"Ü" , "श्" ,
+    "\"k" , "ष" ,
+    "\"" , "ष्" ,
+    "l" , "स" ,
+    "Lk" , "स" ,
+    "L" , "स्" ,
+    "g" , "ह" ,
+    "È" , "ीं" ,
+    "z" , "्र" ,
+    "Ì" , "द्द" ,
+    "Í" , "ट्ट" ,
+    "Î" , "ट्ठ" ,
+    "Ï" , "ड्ड" ,
+    "Ñ" , "कृ" ,
+    "Ò" , "भ" ,
+    "Ó" , "्य" ,
+    "Ô" , "ड्ढ" ,
+    "Ö" , "झ्" ,
+    "Ø" , "क्र" ,
+    "Ù" , "त्त्" ,
+    "¼" , "द्ध" ,
+    "Ú" , "फ्र" ,
+    "É" , "ह्न" ,
 
-"\"k" , "ष" ,
-"\"" , "ष्" ,
-"l" , "स" ,
-"Lk" , "स" ,
-"L" , "स्" ,
-"g" , "ह" ,
-"È" , "ीं" ,
-"z" , "्र" ,
-"Ì" , "द्द" ,
-"Í" , "ट्ट" ,
-"Î" , "ट्ठ" ,
-"Ï" , "ड्ड" ,
-"Ñ" , "कृ" ,
-"Ò" , "भ" ,
-"Ó" , "्य" ,
-"Ô" , "ड्ढ" ,
-"Ö" , "झ्" ,
-"Ø" , "क्र" ,
-"Ù" , "त्त्" ,
-"¼" , "द्ध" ,
-"Ú" , "फ्र" ,
-"É" , "ह्न" ,
-
-// following block added on 19-3-2011
-"Ů" , "त्त्" ,
-"Ľ" , "द्ध" ,
-"˝" , "ऋ" ,
-"Ř" , "क्र" ,
-"Ń" , "कृ" ,
-"Q" , "फ़" ,
-"č" , "ध्" ,
-"Ş" , "्र" ,
-
-
-"\‚" , "ॉ" ,
-"¨" , "ो" ,
-"ks" , "ो" ,
-"©" , "ौ" ,
-"kS" , "ौ" ,
-"k" , "ा" ,
-"h" , "ी" ,
-"q" , "ु" ,
-"w" , "ू" ,
-"\`" , "ृ" ,
-"s" , "े" ,
-"¢" , "े" ,
-"S" , "ै" ,
-"a" , "ं" ,
-"¡" , "ँ" ,
-"ˇ" , "ँ" ,
-"%" , "ः" ,
-"W" , "ॅ" ,
-"•" , "ऽ" ,
-"·" , "ऽ" ,
-"∙" , "ऽ" ,
-"·" , "ऽ" ,
-"+" , "़" ,
-"\\" , "?" ,
-
-"\‘" , "\"" ,
-"\’" , "\"" ,
-"\“" , "\'" ,
-// "\”" , "\'" ,
-
-"^" , "\‘" ,
-"*" , "\’" ,
-"Þ" , "\“" ,
-"ß" , "\”" ,
-"¾" , "=" ,
-"&" , "-" ,
-"μ" , "-" ,
-"¿" , "{" ,
-"À" , "}" ,
-"A" , "।" ,
-// "-" , "." ,
-"Œ" , "॰" ,
-"]" , "\," ,
-"@" , "\/" ,
-
-" ः" , ":" ,
-"~" , "्" ,
-"्ा" ,    "" ,
-"ाे" , "ो" ,
-"ाॅ" , "ॉ" ,
-
-"अौ" , "औ" ,
-"अो" , "ओ" ,
-"आॅ" , "ऑ"    )   
-
-//**********************************************
-
-    var array_one_length = array_one.length ;
-
-    var modified_substring = legacy_txt  ;	
-	
+    // following block added on 19-3-2011
+    "Ů" , "त्त्" ,
+    "Ľ" , "द्ध" ,
+    "˝" , "ऋ" ,
+    "Ř" , "क्र" ,
+    "Ń" , "कृ" ,
+    "Q" , "फ़" ,
+    "č" , "ध्" ,
+    "Ş" , "्र" ,
 
 
-//****************************************************
-//  Break the long text into small bunches of chunk_size  characters each.
-//****************************************************
-    var text_size = modified_substring.length ;
+    "\‚" , "ॉ" ,
+    "¨" , "ो" ,
+    "ks" , "ो" ,
+    "©" , "ौ" ,
+    "kS" , "ौ" ,
+    "k" , "ा" ,
+    "h" , "ी" ,
+    "q" , "ु" ,
+    "w" , "ू" ,
+    "\`" , "ृ" ,
+    "s" , "े" ,
+    "¢" , "े" ,
+    "S" , "ै" ,
+    "a" , "ं" ,
+    "¡" , "ँ" ,
+    "ˇ" , "ँ" ,
+    "%" , "ः" ,
+    "W" , "ॅ" ,
+    "•" , "ऽ" ,
+    "·" , "ऽ" ,
+    "∙" , "ऽ" ,
+    "·" , "ऽ" ,
+    "+" , "़" ,
+    "\\" , "?" ,
 
-    var processed_text = '' ;  //blank
+    "\‘" , "\"" ,
+    "\’" , "\"" ,
+    "\“" , "\'" ,
+    // "\”" , "\'" ,
 
-    var sthiti1 = 0 ;  var sthiti2 = 0 ;  var chale_chalo = 1 ;
- 
-    var chunk_size = 6000; // this charecter long text will be processed in one go.
+    "^" , "\‘" ,
+    "*" , "\’" ,
+    "Þ" , "\“" ,
+    "ß" , "\”" ,
+    "¾" , "=" ,
+    "&" , "-" ,
+    "μ" , "-" ,
+    "¿" , "{" ,
+    "À" , "}" ,
+    "A" , "।" ,
+    // "-" , "." ,
+    "Œ" , "॰" ,
+    "]" , "\," ,
+    "@" , "\/" ,
 
-    while ( chale_chalo == 1 ) 
-    {
-     sthiti1 = sthiti2 ;
+    " ः" , ":" ,
+    "~" , "्" ,
+    "्ा" ,    "" ,
+    "ाे" , "ो" ,
+    "ाॅ" , "ॉ" ,
 
-     if ( sthiti2 < ( text_size - chunk_size ) )  
-     { 
+    "अौ" , "औ" ,
+    "अो" , "ओ" ,
+    "आॅ" , "ऑ");   
+
+  var array_one_length = array_one.length ;  
+  //  Break the long text into small bunches of chunk_size  characters each.
+  var text_size = legacy_txt.length ;
+  var processed_text = '' ;  //blank
+  var sthiti1 = 0 ;  var sthiti2 = 0 ;  var chale_chalo = 1 ;
+  var chunk_size = 6000; // this charecter long text will be processed in one go.
+
+  while ( chale_chalo == 1 ) 
+  {
+    sthiti1 = sthiti2 ;
+    if ( sthiti2 < ( text_size - chunk_size ) )  
+    { 
       sthiti2 +=  chunk_size ;
-     } 
-     else  { sthiti2 = text_size  ;  chale_chalo = 0 }
-
-  var modified_substring = legacy_txt;
-
-  Replace_Symbols( ) ;
-
-  var processed_text = processed_text + modified_substring ;
-	  
-
-    }
+    } 
+    else  { sthiti2 = text_size  ;  chale_chalo = 0 }
+    //alert(legacy_txt);
+    processed_text = processed_text + Replace_Symbols(legacy_txt);
+  }
+  //alert(processed_text);
+  return processed_text;
   
-return processed_text;
+  function Replace_Symbols(modified_substring)
+  {
+    //substitute array_two elements in place of corresponding array_one elements
+    if ( modified_substring == "" )  // if stringto be converted is non-blank then no need of any processing.
+      return;
+    modified_substring = modified_substring.replace( /([ZzsSqwa¡`]+)Q/g , "Q$1" )
+    for ( input_symbol_idx = 0;   input_symbol_idx < array_one_length-1;    input_symbol_idx = input_symbol_idx + 2 )
+    { 
+      idx = 0  ;  // index of the symbol being searched for replacement
 
+      while (idx != -1 ) { //while-00
+        modified_substring = modified_substring.replace( array_one[ input_symbol_idx ] , array_one[input_symbol_idx+1] )
+        idx = modified_substring.indexOf( array_one[input_symbol_idx] )
+      } // end of while-00 loop
+    } // end of for loop
 
-// --------------------------------------------------
-
-
-function Replace_Symbols( )
-
-{
-
-//substitute array_two elements in place of corresponding array_one elements
-
-if ( modified_substring != "" )  // if stringto be converted is non-blank then no need of any processing.
-{
-
-modified_substring = modified_substring.replace( /([ZzsSqwa¡`]+)Q/g , "Q$1" )
-
-for ( input_symbol_idx = 0;   input_symbol_idx < array_one_length-1;    input_symbol_idx = input_symbol_idx + 2 )
-
-{ 
-//******************************************************
-idx = 0  ;  // index of the symbol being searched for replacement
-
-while (idx != -1 ) //while-00
-{
-
-modified_substring = modified_substring.replace( array_one[ input_symbol_idx ] , array_one[input_symbol_idx+1] )
-idx = modified_substring.indexOf( array_one[input_symbol_idx] )
-
-} // end of while-00 loop
-
-} // end of for loop
-
-modified_substring = modified_substring.replace( /([ेैुूं]+)्र/g , "्र$1" ) ;
-modified_substring = modified_substring.replace( /ं([ाेैुू]+)/g , "$1ं" ) ;
-
-modified_substring = modified_substring.replace( /([ \n])ा/g , "$1श" ) ;
-
-
-modified_substring = modified_substring.replace( /¯/g , "f") ;
-modified_substring = modified_substring.replace( /Ł/g , "र्f") ;	
-	
-modified_substring = modified_substring.replace( /([fŻ])([कखगघङचछजझञटठडड़ढढ़णतथदधनपफबभमयरलवशषसहक्ष])/g , "$2$1" ) ;
-
-modified_substring = modified_substring.replace( /([fŻ])(्)([कखगघङचछजझञटठडड़ढढ़णतथदधनपफबभमयरलवशषसहक्ष])/g , "$2$3$1" ) ;
+    modified_substring = modified_substring.replace( /([ेैुूं]+)्र/g , "्र$1" ) ;
+    modified_substring = modified_substring.replace( /ं([ाेैुू]+)/g , "$1ं" ) ;
+    modified_substring = modified_substring.replace( /([ \n])ा/g , "$1श" ) ;
+    modified_substring = modified_substring.replace( /¯/g , "f") ;
+    modified_substring = modified_substring.replace( /Ł/g , "र्f") ;	
+    modified_substring = modified_substring.replace( /([fŻ])([कखगघङचछजझञटठडड़ढढ़णतथदधनपफबभमयरलवशषसहक्ष])/g , "$2$1" ) ;
+    modified_substring = modified_substring.replace( /([fŻ])(्)([कखगघङचछजझञटठडड़ढढ़णतथदधनपफबभमयरलवशषसहक्ष])/g , "$2$3$1" ) ;
+    modified_substring = modified_substring.replace( /([fŻ])(्)([कखगघङचछजझञटठडड़ढढ़णतथदधनपफबभमयरलवशषसहक्ष])/g , "$2$3$1" ) ;
+    modified_substring = modified_substring.replace( /f/g , "ि") ;
+    modified_substring = modified_substring.replace( /Ż/g , "िं") ;    
+    //following three statement for adjusting position of reph ie, half r .
+    modified_substring = modified_substring.replace( /±/g , "Zं" ) ;
+    modified_substring = modified_substring.replace( /([कखगघचछजझटठडड़ढढ़णतथदधनपफबभमयरलळवशषसहक्षज्ञ])([ािीुूृेैोौंँ]*)([Z])/g , "$3$1$2" ) ;
+    modified_substring = modified_substring.replace( /([कखगघचछजझटठडड़ढढ़णतथदधनपफबभमयरलळवशषसहक्षज्ञ])([्])([Z])/g , "$3$1$2" ) ;
+    modified_substring = modified_substring.replace( /Z/g , "र्" ) ;  
     
-modified_substring = modified_substring.replace( /([fŻ])(्)([कखगघङचछजझञटठडड़ढढ़णतथदधनपफबभमयरलवशषसहक्ष])/g , "$2$3$1" ) ;
-
-modified_substring = modified_substring.replace( /f/g , "ि") ;
-modified_substring = modified_substring.replace( /Ż/g , "िं") ;    
-
-	
-//following three statement for adjusting position of reph ie, half r .
-modified_substring = modified_substring.replace( /±/g , "Zं" ) ;
-
-modified_substring = modified_substring.replace( /([कखगघचछजझटठडड़ढढ़णतथदधनपफबभमयरलळवशषसहक्षज्ञ])([ािीुूृेैोौंँ]*)([Z])/g , "$3$1$2" ) ;
-
-modified_substring = modified_substring.replace( /([कखगघचछजझटठडड़ढढ़णतथदधनपफबभमयरलळवशषसहक्षज्ञ])([्])([Z])/g , "$3$1$2" ) ;
-
-modified_substring = modified_substring.replace( /Z/g , "र्" ) ;
-
-} // end of IF  statement  meant to  supress processing of  blank  string.
-
-} // end of the function  Replace_Symbols
-
+    return modified_substring;
+  } // end of the function  Replace_Symbols
 } // end of convert_to_unicode function
-
 
 function convert_chanakya_to_unicode(legacy_txt) {
   var array_one = new Array(
@@ -665,7 +642,6 @@ function Devlys2Uni(text) {
   // "f" )
   // "ि"  
 
-
   var array_one_length = array_one.length ;
   var modified_substring = text  ;
 
@@ -787,6 +763,3 @@ function Devlys2Uni(text) {
     } // end of IF  statement  meant to  supress processing of  blank  string.
   } // end of the function  Replace_Symbols
 } // end of Kritidev_to_unicode function
-
-
-
