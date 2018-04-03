47e1b6a34169976517554e15f3d3822955d5e57b
Himanshu Garg <himanshugarg@zoho.com>
2018-04-03 11:58:06 +0530

    Removed verifyfolder function to create files in the same folder

--- pmdimport.jsx
+++ pmdimport.jsx
@@ -1,30 +1,26 @@
+// check version
 var myInDesignVersion = Number(String(app.version).split(".")[0]);  
-  
+if (myInDesignVersion < 5 || myInDesignVersion > 8) {  
+    alert("This script is for InDesign CS3 - CS6.", "Batch resave PageMaker files script");  
+    exit();  
+}  
+
+// select pmd files  
 var myFolder = Folder.selectDialog("Select a folder with PageMaker files to resave");  
 if (myFolder == null) exit();  
 var myFilelist = [];  
 var myAllFilesList = myFolder.getFiles();  
-  
 for (var f = 0; f < myAllFilesList.length; f++) {  
     var myFile = myAllFilesList[f];  
     if (myFile instanceof File && myFile.name.match(/\.pmd$/i)) {  
         myFilelist.push(myFile);  
     }  
 }  
-if (myInDesignVersion < 5 || myInDesignVersion > 8) {  
-    alert("This script is for InDesign CS3 - CS6.", "Batch resave PageMaker files script");  
-    exit();  
-}  
-  
 if (myFilelist.length == 0) {  
     alert("No files to open.", "Batch resave PageMaker files script");  
     exit();  
 }  
-  
-var myResavedFolder = new Folder( myFolder.fsName + "/Resaved Files/" );  
-VerifyFolder(myResavedFolder);  
-  
-var myCounter = 1;  
+ 
 app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;  
   
 // Progress bar -----------------------------------------------------------------------------------  
@@ -38,52 +34,24 @@ myProgressWin.show();
   
 for (var i = myFilelist.length-1; i >= 0; i--) {  
     var myCurrentFile = myFilelist[i];  
-      
+    
     try {  
         var myDoc = app.open(myCurrentFile, false);  
         var myDocName = myCurrentFile.name.replace(/pmd$/i, "indd");  
-        var myDocFilePath = new File(myResavedFolder.fsName + "/" + myDocName);  
-  
+        var myDocFilePath = new File(myFolder.fsName + "/" + myDocName);   
+
         // Progress bar -----------------------------------------------------------------------------------  
-        myProgressBar.value = myCounter;  
-        myProgressTxt.text = String("Resaving file - " + myDocName + " (" + myCounter + " of " + myFilelist.length + ")");  
+        myProgressBar.value = myFilelist.length-i;  
+        myProgressTxt.text = String("Resaving file - " + myDocName + " (" + myProgressBar.value + " of " + myFilelist.length + ")");  
         // Progress bar -----------------------------------------------------------------------------------  
-  
+    
         myDoc = myDoc.save(myDocFilePath);  
         myDoc.close();  
-        myCounter++;  
     }  
     catch(e) {}  
 }  
-  
-    // Progress bar -----------------------------------------------------------------------------------  
-    myProgressWin.close();  
-    // Progress bar -----------------------------------------------------------------------------------  
-  
+// Progress bar -----------------------------------------------------------------------------------  
+myProgressWin.close();  
+// Progress bar -----------------------------------------------------------------------------------  
 app.scriptPreferences.userInteractionLevel = UserInteractionLevels.INTERACT_WITH_ALL;  
-  
-alert("Done.", "Batch resave PageMaker files script");  
-  
-//--------------------------------------------------------------------------------------------------------------  
-function VerifyFolder(myFolder) {  
-    if (!myFolder.exists) {  
-        var myFolder = new Folder(myFolder.absoluteURI);  
-        var myArray1 = new Array();  
-        while (!myFolder.exists) {  
-            myArray1.push(myFolder);  
-            myFolder = new Folder(myFolder.path);  
-        }  
-        myArray2 = new Array();  
-        while (myArray1.length > 0) {  
-            myFolder = myArray1.pop();  
-            if (myFolder.create()) {  
-                myArray2.push(myFolder);  
-            } else {  
-                while (myArray2.length > 0) {  
-                    myArray2.pop.remove();  
-                }  
-                throw "Folder creation failed";  
-            }   
-        }  
-    }  
-}  
+alert("Done.", "Batch resave PageMaker files script");   
