ad7e6a07fdae55e56871f78235351341827d4c2c
Himanshu Garg <himanshugarg@zoho.com>
2018-05-18 19:03:16 +0530

    refactored heading code and added Sankalan's scaling script

--- heading.jsx
+++ heading.jsx
@@ -1,13 +1,12 @@
 ﻿// object model initialisations
-var myTextFrames  = app.activeDocument.textFrames;
-var myDoc         = app.documents[0];
-var avg_font_size = iterate(averager);
+var avg_font_size = apply_on_text_elements(averager);
 
 // all texts bigger than avg_font_size
-var headings      = iterate(style_creator);
+var headings      = apply_on_text_elements(style_creator);
+
 // headings_for_level is a 2D array which contains 6 rows -
 // one for each heading level. And each row will contain the
-// paragraph objects correspnding to that level
+// paragraph objects corresponding to that level
 var headings_for_level = new Array(6);
 for (var i = 0; i < 6; i++) {
   headings_for_level[i] = new Array();
@@ -16,58 +15,45 @@ for (var i = 0; i < 6; i++) {
 // assuming the first word/text frame is the title and hence h1
 headings_for_level[0].push(headings[0]);
 headings[0].level = 0;
-for (var i = 1; headings[i]!=null; i++) {
-  //if size is equal, same level
-  if (headings[i].size == headings[i-1].size) {
-    headings[i].level = headings[i-1].level;
-  } else if (headings[i].size > headings[i-1].size) { //if size is greater, heading level will be above the previous one
-    if (headings[i-1].level == 0) { //if the previous heading level was h1, there is nothing above it, so same as before
-      headings[i].level = headings[i-1].level;
-    } else {
-      var k = headings[i-1].level;
-      while(headings[i].size>=headings_for_level[k-1][headings_for_level[k-1].length-1].size) { //VUDO >=
-                                                        // if size is greater, keep checking with higher heading headings_for_level
-                                                        // until you reach a level which has a higher fontsize,
-                                                        // and then level is assigned
-        k--;
-        if (k==0) break;
-      }
-      headings[i].level = k;
-    }
-  } else { //if size is smaller, one heading level below
-    if (headings[i-1].level == 5) { //if prev level is h6, then there is no lower heading level
-      headings[i].level = headings[i-1].level;
-    } else {
-      headings[i].level = headings[i-1].level + 1;
-    }
+for (var i = 1; headings[i]!=null; i++) {  
+  if (headings[i].size < headings[i-1].size) {                                  // smaller than previous
+    headings[i].level = headings[i-1].level + 1;
+  } else if ((headings[i].size > headings[i-1].size && headings[i-1].level == 0)// bigger than previous but previous already h1
+  || (headings[i].size == headings[i-1].size)                                   // same as previous
+  || (headings[i].size < headings[i-1].size && headings[i-1].level == 5)) {     // smaller than previous but previous already h5
+    headings[i].level = headings[i-1].level;    
+  } else {                                                                      // look for the right heading level
+    var k = headings[i-1].level;
+    while(k > 0 && headings[i].size >= headings_for_level[k][headings_for_level[k].length-1].size)
+      k--;          
+    headings[i].level = k+1;
   }
   headings_for_level[headings[i].level].push(headings[i]);
   write_to_file("\nAssiging " + headings[i].text.contents + " to level " + headings[i].level);
 }
 
-for (var i=0;i<6;i++) { //iterate over the rows in headings_for_level
-  for (var j=0; j<headings_for_level[i].length;j++) { //iterate over alll elemets of one row
-    headings_for_level[i][j].text.appliedParagraphStyle = myDoc.paragraphStyles.item (headings_for_level[i][j].style.name); //apply the custom paragraph style created to the paragraph
+for (var i=0;i<6;i++) { //apply_on_text_elements over the rows in headings_for_level
+  for (var j=0; j<headings_for_level[i].length;j++) { //apply_on_text_elements over alll elemets of one row
+    headings_for_level[i][j].text.appliedParagraphStyle = app.documents[0].paragraphStyles.item (headings_for_level[i][j].style.name); //apply the custom paragraph style created to the paragraph
     //according to the heading level of the paragraph, assign an export tag to the custom style created for each respective paragraph
-    var h = myDoc.paragraphStyles.itemByName(headings_for_level[i][j].style.name);
+    var h = app.documents[0].paragraphStyles.itemByName(headings_for_level[i][j].style.name);
     h.styleExportTagMaps.add({exportType: "EPUB", exportTag: "h"+(i+1), exportClass: "", exportAttributes: ""});
     h.styleExportTagMaps.add({exportType: "PDF", exportTag: "H"+(i+1), exportClass: "", exportAttributes: ""});
   }
 }
 
-function iterate(fun) {
-  var o = fun();
-  // two while loops iterate over all paragraphs present in the object model
-  for (var i = 0; i < app.activeDocument.textFrames.length;  i++) {
-    var paras=myTextFrames[i].parentStory.paragraphs;
-    myTextPrev = paras.item(0);
-    for (var j = -1; j < paras.count() && myTextFrames[i].parentStory!= myTextFrames[i-1].parentStory; j++) { //VUDO j = -1
-      myText = paras.item(j);
-      if (myTextPrev == null || myText != myTextPrev) { // to avoid repetition
-        o.fun(myText);
-      }
-      myTextPrev = myText;
-    }
+function apply_on_text_elements(fun) {
+  var o = fun();    
+  var stories = app.activeDocument.stories.everyItem().getElements();
+
+  for (var i = 0; i < stories.length; i++) {
+    var textStyleRanges = stories[i].textStyleRanges.everyItem().getElements();
+    
+    for (var j = 0; j < textStyleRanges.length; j++) {
+      var myText = textStyleRanges[j];   
+      write_to_file(myText.contents + "\n");
+      o.fun(myText)      
+    }  
   }
   return o.val();
 }
@@ -92,11 +78,14 @@ function style_creator() {
     fun: function(myText) {
             if (myText.pointSize > avg_font_size) {  // distinguish between non heading and heading text by using the average
               // create an object having the paragraph text, its size, heading number, and a new custom style which is different for each paragraph, and given the various style parameters to maintain formatting
-              var name = headings.length.toString();
+              
+              var styleName = "autogenerated_" + headings.length.toString()
+              write_to_file(myText.contents + " (name): " + styleName);
+              write_to_file(myText.contents + " (fontStyle): " + myText.fontStyle);
               headings.push({ text : myText ,
                               size : myText.pointSize ,
                               level : 0 ,
-                              style : myDoc.paragraphStyles.add({ name : name,
+                              style : app.documents[0].paragraphStyles.add({ name : styleName,
                                                                   spaceAfter : myText.spaceAfter ,
                                                                   appliedFont : myText.appliedFont,
                                                                   fontFamily : myText.appliedFont.fontFamily,
@@ -120,7 +109,7 @@ function style_creator() {
                                                                   justification : myText.justification
                 })
               });
-              write_to_file("headings[" + (headings.length-1) + "]:" + myText.contents + ", " + myText.pointSize);
+              //write_to_file("headings[" + (headings.length-1) + "]: (" + myText.contents + "), " + myText.pointSize);
             }
     },
 
@@ -131,7 +120,7 @@ function style_creator() {
 }
 
 function write_to_file(text) {
-  var file = new File("~/Desktop/heading2.txt");
+  var file = new File("~/Desktop/heading.txt");
   file.encoding = "UTF-8";
   if (file.exists) {
     file.open("e");
