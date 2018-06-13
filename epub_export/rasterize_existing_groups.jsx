4b351f6c544c7ef5cf33b38d932047993f944ff4
Dwijesh522 <iddwiejsh@gmail.com>
2018-06-13 12:23:58 +0530

    rasterize only existing groups in the document

---
+++ epub_export/rasterize_existing_groups.jsx
@@ -0,0 +1,11 @@
+﻿        // this script serches for all GROUPES in active document and checks the option Custom Rasterization( customImageConversion ) to rasterize the group.
+        
+        var myDocument = app.activeDocument;
+        myGroups = myDocument.groups;
+        alert( myGroups.length );
+        
+        var i;
+        for( i=0 ; i<myGroups.length ; i++ )
+        {
+                myGroups[i].objectExportOptions.customImageConversion = true ;
+        }
